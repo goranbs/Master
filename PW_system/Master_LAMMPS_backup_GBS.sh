3811173c32a58a0057fc1f12f605f21672ca1413
goranbs <g.b.svaland@fys.uio.no>
2014-10-13 15:26:53 +0200

    adding backupscript

---
+++ PW_system/Master_LAMMPS_backup_GBS.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+###################################################################
+#
+# BACKUP script of lammps-generated files for masterproject
+#
+# Masterproject in molecular dynamics for Goeran Brekke Svaland
+#
+###################################################################
+
+# What to back up
+backup_files="/home/goran/lammps-28Jun14/examples/water_portlandite_system"
+
+# Where to back up to
+dest="/media/goran/9196bccb-acd5-42ba-ae64-4231e568fd1d/home/goran/LAMMPS_backup"
+
+# Create archive filename
+day=$(date +%F)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Back up the files using tar. c-create v-verbose z-gzip f-archive (create archive using gzip verbosly)
+tar cvzf $dest/$archive_file $backup_files
+
+# Print end status message
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes
+ls -lh $dest
+
+
